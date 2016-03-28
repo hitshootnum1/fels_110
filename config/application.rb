f31985b5ae35cc52a670ccf50a38c782f7b2cef9
Tran Tien Duc <hitshootnum1@gmail.com>
2016-03-28 11:44:31 +0700

    Finish Static Pages

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path("../boot", __FILE__)
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +22,6 @@ module BaseApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
   end
 end
