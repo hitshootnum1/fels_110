9d77dc79f4a23cf79a53e4cf8cb8f36b6351075f
Tran Tien Duc <hitshootnum1@gmail.com>
2016-03-30 18:10:42 +0700

    Finish category show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,8 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.paginate page: params[:page]
   end
+
+  def show
+    @category = Category.find_by id: params[:id]
+  end
 end
